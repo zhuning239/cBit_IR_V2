4010c86b897801c13047270bc0536262c924eebf
zhuning239 <364711106@qq.com>
2021-01-06 12:41:48 +0800

    Update cBit-IRV2.ts

--- cBit-IRV2.ts
+++ cBit-IRV2.ts
@@ -18,6 +18,50 @@ namespace cBitIR
         public mye: string;
         public myparam: number;
     }
+    export enum enIRkey{
+      //% blockId="CH_MINUS" block="CH_MINUS"
+      CH_MINUS = 0x00,
+      //% blockId="CH" block="CH"
+	  CH = 0x01,
+      //% blockId="CH_Add" block="CH_Add"
+	  CH_Add = 0x02,
+      //% blockId="PREV" block="PREV"
+	  PREV = 0x03,
+      //% blockId="PLAY" block="PLAY"
+	  PLAY = 0x04,
+      //% blockId="NUM_200" block="NUM_200"
+	  NUM_200 = 0x05,
+      //% blockId="NEXT" block="NEXT"
+	  NEXT = 0x06,
+      //% blockId="NUM_100" block="NUM_100"
+	  NUM_100 = 0x07,
+      //% blockId="Minus" block="Minus"
+	  Minus = 0x08,
+      //% blockId="Add" block="Add"
+	  Add = 0x09,
+      //% blockId="EQ" block="EQ"
+	  EQ = 0x0a,
+      //% blockId="NUM0" block="NUM0"
+	  NUM0 = 0x0b,
+      //% blockId="NUM1" block="NUM1"
+	  NUM1 = 0x0c,
+      //% blockId="NUM2" block="NUM2"
+	  NUM2 = 0x0d,
+      //% blockId="NUM3" block="NUM3"
+	  NUM3 = 0x0e,
+      //% blockId="NUM4" block="NUM4"
+	  NUM4 = 0x0f,
+      //% blockId="NUM5" block="NUM5"
+	  NUM5 = 0x10,
+      //% blockId="NUM6" block="NUM6"
+	  NUM6 = 0x11,
+      //% blockId="NUM7" block="NUM7"
+	  NUM7 = 0x12,
+      //% blockId="NUM8" block="NUM8"
+	  NUM8 = 0x13,
+      //% blockId="NUM9" block="NUM9"
+	  NUM9 = 0x14,
+    }
     /**
      * Read IR sensor value V2.
      */
@@ -25,6 +69,13 @@ namespace cBitIR
     //% advanced=true shim=maqueenIRV2::irCode
     function irCode(): number {
         return 0;
+    }
+    
+    //% weight=5
+    //% group="micro:bit(V2)"
+    //% blockId=clb_IR_KeyValue block="clb_IR_KeyValue|value %value"
+    export function clb_IR_KeyValue(value: enIRkey): number {
+        return value;
     }
     
     //% weight=5
